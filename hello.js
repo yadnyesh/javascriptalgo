f895427a9029e138ad9f9b10ff9e40b4a10b7d75
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-01-08 11:56:58 +0530

    Lecture 5: performance measurement

---
+++ hello.js
@@ -0,0 +1,18 @@
+const performance = require('perf_hooks').performance;
+
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 1; i <=n; i++) {
+        total += i;
+    }
+    return total;
+}
+
+function addUpToNoLoop(n) {
+    return n * (n+1)/2;
+}
+
+let t1 = performance.now()
+console.log(addUpToNoLoop(100));
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1)/1000} seconds`)
